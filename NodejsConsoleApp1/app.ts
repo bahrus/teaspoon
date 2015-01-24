901e290690e1dd19321446c13db65fd5eb22cd28
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2015-01-23 23:18:45 -0500

    tsp

---
+++ NodejsConsoleApp1/app.ts
@@ -0,0 +1,9 @@
+﻿///<reference path='testModule.ts'/>
+
+var testModule = require('./testModule');
+global['myModule'] = testModule['testModule'];
+for (var key in testModule) {
+    console.log('testModule.' + key + ' = ' + testModule[key]);
+}
+console.log(myModule.I12);
+console.log('iah');
---
+++ NodejsConsoleApp1/testModule.ts
@@ -0,0 +1,10 @@
+﻿module myModule {
+    export var I12 = 'hello';
+}
+
+exports.testModule = myModule;
+
+declare module "testModule" {
+    export = myModule;
+}
+
