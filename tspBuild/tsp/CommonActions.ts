bbf572a4afc049c1c05743f75df7866f9bc61dab
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2015-01-24 10:48:22 -0500

    tsp

--- tspBuild/tsp/CommonActions.ts
+++ tspBuild/tsp/CommonActions.ts
@@ -1,3 +1,10 @@
 ﻿module tsp.CommonActions {
     var versionKey = 'version';
 }
+
+if (typeof (global) !== 'undefined') {
+    var guid = 'tsp-81B44259-976C-4DFC-BE00-6E901415FEF3';
+    var globalNS = global[guid] || 'tsp';
+    if (!global[globalNS]) global[globalNS] = {};
+    global[globalNS].CommonActions = tsp.CommonActions;
+}
---
+++ tspBuild/tsp/ParserActions.ts
@@ -0,0 +1,31 @@
+﻿module tsp.ParserActions {
+    //#region Helper functions
+    export function endsWith(str: string, suffix: string) {
+        return str.indexOf(suffix, str.length - suffix.length) !== -1;
+    }
+
+    export function startsWith(str: string, prefix: string) {
+        return str.substr(0, prefix.length) === prefix;
+    }
+
+    export function replaceStartWith(str: string, prefix: string, replaceText: string) {
+        if (!startsWith(str, prefix)) return str;
+        return str.substr(prefix.length);
+    }
+
+    export function replaceEndWith(str: string, suffix: string, replaceText: string) {
+        var iPosOfEnd = str.indexOf(suffix, str.length - suffix.length);
+        if (iPosOfEnd === -1) return str;
+        return str.substr(0, iPosOfEnd) + replaceText;
+    }
+
+
+//#endregion
+}
+
+if (typeof (global) !== 'undefined') {
+    var guid = 'tsp-81B44259-976C-4DFC-BE00-6E901415FEF3';
+    var globalNS = global[guid] || 'tsp';
+    if (!global[globalNS]) global[globalNS] = {};
+    global[globalNS].ParserActions = tsp.ParserActions;
+}
