6e041849f7f34d31ceee09eb9873fe4d1515e82e
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2015-01-25 10:14:38 -0500

    tsp

--- tspBuild/tsp/Refs.ts
+++ tspBuild/tsp/Refs.ts
@@ -3,6 +3,7 @@
     require('./ParserActions');
     require('./TypeScriptEntities');
     require('./FileSystemActions');
+    require('./DOMActions');
     var _ = <_.LoDashStatic> require('lodash');
     global._ = _;
     var cheerio = require('cheerio');
