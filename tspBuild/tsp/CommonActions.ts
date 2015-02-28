567ff61bec78976813f220d0fce2940b68e575f3
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2015-02-28 14:45:36 -0500

    tsp

--- tspBuild/tsp/CommonActions.ts
+++ tspBuild/tsp/CommonActions.ts
@@ -182,8 +182,8 @@
 }
 
 if (typeof (global) !== 'undefined') {
-    var guid = 'tsp-81B44259-976C-4DFC-BE00-6E901415FEF3';
-    var globalNS = global[guid] || 'tsp';
-    if (!global[globalNS]) global[globalNS] = {};
-    global[globalNS].CommonActions = tsp.CommonActions;
+    //var guid = 'tsp-81B44259-976C-4DFC-BE00-6E901415FEF3';
+    //var globalNS = global[guid] || 'tsp';
+    global.refs.nameTBD = ['CommonActions', tsp.CommonActions];
+    //global[globalNS].CommonActions = tsp.CommonActions;
 }
--- tspBuild/tsp/Refs.ts
+++ tspBuild/tsp/Refs.ts
@@ -3,17 +3,23 @@
     global._ = _;
     var cheerio = require('cheerio');
     global.cheerio = cheerio;
+    var guid = 'tsp-81B44259-976C-4DFC-BE00-6E901415FEF3';
+    var globalNS = global[guid] || 'tsp';
     if (!global.refs) {
         global.refs = {
             obj: Object,
             set moduleTarget(obj) {
+                if (!global[globalNS]) global[globalNS] = {};
                 for (var key in global.tsp) {
-                    if (!obj[key]) obj[key] = global.tsp[key];
+                    if (!obj[key]) obj[key] = global[globalNS][key];
                 }
                 this.obj = obj;
             },
             get moduleTarget() {
                 return this.obj;
+            },
+            set nameTBD(arr) {
+                global[globalNS][arr[0]] = arr[1];
             }
         };
     }
