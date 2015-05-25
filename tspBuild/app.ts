078fcbec2ac2f052b91d82a88de37a1a22548b11
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2015-05-25 15:26:31 -0400

    var => let, const

--- tspBuild/app.ts
+++ tspBuild/app.ts
@@ -1,24 +1,24 @@
 ﻿///<reference path='tsp/NodeJSImplementations.ts'/>
-var guid2 = 'i am here';
+const guid2 = 'i am here';
 if (typeof (global) !== 'undefined') {
     require('./tsp/Refs');
 }
-//var ca1 = tsp.CommonActions;
-//var pa1 = tsp.ParserActions;
+//const ca1 = tsp.CommonActions;
+//const pa1 = tsp.ParserActions;
 //console.log(pa1.replaceEndWith(ca1.versionKey, 'n', 'x'));
-//var array = [1, 2, 3, 4, 5];
+//const array = [1, 2, 3, 4, 5];
 //_.each(array, n => console.log(n));
 //console.log('iah');
-var nji = tsp.NodeJSImplementations;
-var ca = tsp.CommonActions;
-var bc = tsp.BuildConfig;
-var context: tsp.FileSystemActions.IWebContext = {
+const nji = tsp.NodeJSImplementations;
+const ca = tsp.CommonActions;
+const bc = tsp.BuildConfig;
+const context: tsp.FileSystemActions.IWebContext = {
     stringCache: {},
     HTMLOutputs: {},
     fileManager: new nji.NodeJSWebFileManager(),
     processManager: new nji.NodeJSProcessManager(),
 };
-var callback: tsp.CommonActions.ICallback = (err) => {
+const callback: tsp.CommonActions.ICallback = (err) => {
 };
 
 bc.programConfig.do(bc.programConfig, context, callback);
--- tspBuild/tsp/Refs.ts
+++ tspBuild/tsp/Refs.ts
@@ -1,15 +1,15 @@
 ﻿if (typeof (global) !== 'undefined') {
-    var _ = <_.LoDashStatic> require('lodash');
+    const _ = <_.LoDashStatic> require('lodash');
     global._ = _;
     var cheerio = require('cheerio');
     global.cheerio = cheerio;
-    var guid = 'tsp-81B44259-976C-4DFC-BE00-6E901415FEF3';
-    var globalNS = global[guid] || 'tsp';
+    const guid = 'tsp-81B44259-976C-4DFC-BE00-6E901415FEF3';
+    const globalNS = global[guid] || 'tsp';
     if (!global.refs) {
         global.refs = {
             set moduleTarget(obj) {
                 if (!global[globalNS]) global[globalNS] = {};
-                for (var key in global.tsp) {
+                for (const key in global.tsp) {
                     if (!obj[key]) obj[key] = global[globalNS][key];
                 }
             },
